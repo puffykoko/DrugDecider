b6e7ebebbeb764df5aae4576f84c7440d3978067
Lucy Huo <lucendary@ucla.edu>
2019-10-25 17:53:17 -0700

    created some pages

---
+++ src/pages/FormPage.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function FormPage() {
+  return (
+    <div>
+      <p>This is the form page.</p>
+    </div>
+  );
+}
+
+export default FormPage;
---
+++ src/pages/ResultsPage.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function ResultsPage() {
+  return (
+    <div>
+      <p>This is the results page.</p>
+    </div>
+  );
+}
+
+export default ResultsPage;
