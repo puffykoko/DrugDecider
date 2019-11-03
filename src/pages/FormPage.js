7b2ebc6ea5d8ed4d9e79693ae5cfd97db0b73c6d
Lucy Huo <lucendary@ucla.edu>
2019-11-03 01:50:29 -0800

    created Footer and added some responsiveness

--- src/pages/FormPage.js
+++ src/pages/FormPage.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function FormPage() {
   return (
-    <div>
+    <div className="content flex">
       <p>This is the form page.</p>
     </div>
   );
--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -5,28 +5,26 @@ import "./LandingPage.css";
 
 function LandingPage() {
   return (
-    <div className="container">
-      <div className="content flex">
-        <div className="leftContent">
-          <div className="text">
-            <h2 className="largeText">Find what works for you.</h2>
-            <p>
-              Drug Decider helps patients find the right medication by analyzing
-              their medical history. Lorem ipsum dolor sit amet, consectetur
-              adipiscing elit. Sed suscipit, massa sit amet euismod scelerisque.
-            </p>
-          </div>
-          <Link to="/form" className="startButton">
-            Let's get started!
-          </Link>
+    <div className="content flex" id="content-wrap">
+      <div className="leftContent">
+        <div className="text">
+          <h2 className="largeText">Find what works for you.</h2>
+          <p>
+            Drug Decider helps patients find the right medication by analyzing
+            their medical history. Lorem ipsum dolor sit amet, consectetur
+            adipiscing elit. Sed suscipit, massa sit amet euismod scelerisque.
+          </p>
         </div>
-        <div className="imageContent flex">
-          <img
-            src="https://nathanbrixius.files.wordpress.com/2013/02/image3.png"
-            id="landingImage"
-            alt="Graph of results"
-          />
-        </div>
+        <Link to="/form" className="startButton">
+          Let's get started!
+        </Link>
+      </div>
+      <div className="imageContent flex">
+        <img
+          src="https://nathanbrixius.files.wordpress.com/2013/02/image3.png"
+          id="landingImage"
+          alt="Graph of results"
+        />
       </div>
     </div>
   );
