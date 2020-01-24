a8b0ce82a77dd1dbf767db5c2b9a5f7898e37cc0
Matthew Ko <mk9917@yahoo.com>
2020-01-23 21:36:07 -0800

    stylized form pages

---
+++ src/components/ProgressBar.js
@@ -0,0 +1,36 @@
+import React from "react";
+
+function ProgressBar(props) {
+  var i = 0, arr = [];
+  for(i = 0; i < props.numBars; i += 1){
+    arr[i] = i;
+  }
+
+  const mapped_data = arr.map((index) => (
+      <div style={{
+        width: "100%",
+        height: 10,
+        margin: "0px 10px",
+        backgroundColor: index === props.curBar ? "#69B8F1" : "#DCDCDC",
+        transition: "background-color 1s",
+      }}>
+
+      </div>
+    ));
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "space-around",
+      }}
+    >
+
+    {mapped_data}
+
+    </div>
+  );
+}
+
+
+export default ProgressBar;
